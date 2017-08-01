0fb55731c856a6dd435b65cd7ca1a855d976a161
Eddie <itein@Hotmail.com>
2017-07-31 22:35:40 -0700

    xfer to work on desktop

---
+++ train.js
@@ -0,0 +1,13 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyDfM5tiQsfZi3Ai0og41MVuV1sNrNdUC4E",
+    authDomain: "bootcamp-e905f.firebaseapp.com",
+    databaseURL: "https://bootcamp-e905f.firebaseio.com",
+    projectId: "bootcamp-e905f",
+    storageBucket: "bootcamp-e905f.appspot.com",
+    messagingSenderId: "540996318964"
+  };
+firebase.initializeApp(config);
+var database = firebase.database();
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
